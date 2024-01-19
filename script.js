d8fb2387ccf52d5a5c78b83f0d99906bf9bc4a13
pandeleo <pandelea.leo@gmail.com>
2024-01-19 16:33:33 +0000

    Files Modified

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const button = document.querySelector('button');
 const title = document.querySelector('h1');
 
-button.addEventListener('click', function(){
-    title.style.color = "red";
-})
+button.addEventListener('click', async function(){
+    const colors = ['red', 'blue', 'green', 'gray', 'gold', 'silver', 'orange', 'aquamarine'];
+
+    for(let i = 0; i < colors.length; i++){
+        await new Promise(resolve => setTimeout(resolve, 1000)); // Pauza de o secundă între schimbările de culoare
+        title.style.color = colors[i];
+    }
+});
